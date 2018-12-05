864285576443f97e9668a4462795374fc84fe026
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-05 15:48:34 -0500

    Change name home to 8 Ball Radio

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ import AppHeader from "./components/layout/AppHeader.vue";
 import AppFooter from "./components/layout/AppFooter.vue";
 
 export default {
-  name: "home",
+  name: "8 Ball Radio",
   components: {
     "app-header": AppHeader,
     "app-footer": AppFooter
