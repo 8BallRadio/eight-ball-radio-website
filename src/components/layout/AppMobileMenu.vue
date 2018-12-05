fefd269ba26526263f8fcb8b9e0cec78beacafe0
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-05 15:54:03 -0500

    Initial AppMobileMenu.vue file

---
+++ src/components/layout/AppMobileMenu.vue
@@ -0,0 +1,67 @@
+<template>
+  <aside id="mobile-menu" class="mobile-menu">
+    <div class="overlay"></div>
+    <div class="nav-content">
+      <button class="close-menu btn" @click="closeMenu">CLOSE X</button>
+      <nav class="nav__items">
+        <a href class="nav__link">SHOWS</a>
+        <a href class="nav__link">SCHEDULE</a>
+        <a href class="nav__link">EVENTS</a>
+        <a href class="nav__link">ABOUT</a>
+        <a href class="nav__link">8-BALL FAMILY</a>
+        <a href class="nav__link">SHOP</a>
+      </nav>
+      <div class="top__social">
+        <a href="https://www.facebook.com/eightballradio/" target="_blank">
+          <img
+            class="social__icon"
+            src="../../assets/header/facebook.svg"
+            onerror="this.src='../../assets/header/facebook.png'"
+            alt="Facebook Fan Page"
+          >
+        </a>
+        <a href="https://www.instagram.com/8ballradio/" target="_blank">
+          <img
+            class="social__icon"
+            src="../../assets/header/instagram.svg"
+            onerror="this.src='../../assets/header/instagram.png'"
+            alt="Instagram"
+          >
+        </a>
+        <a href="https://www.mixcloud.com/8ballradio/" target="_blank">
+          <img
+            class="social__icon"
+            src="../../assets/header/mixcloud.svg"
+            onerror="this.src='../../assets/header/mixcloud.png'"
+            alt="Mixcloud"
+          >
+        </a>
+      </div>
+    </div>
+  </aside>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isSideBarOpen: false
+    };
+  },
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: false
+    }
+  },
+  methods: {
+    closeMenu() {
+      this.$emit("closeMenu");
+      this.isSideBarOpen = false;
+      document.querySelector("#mobile-menu").className = document
+        .querySelector("#mobile-menu")
+        .className.replace("active", "");
+      document.body.style.overflow = "unset";
+    }
+  }
+};
+</script>
