5eae55839745aee28684b6eff2ac214f93a79a82
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-11 05:25:12 -0500

    Create Shows.vue and _shows.scss files

---
+++ src/views/Shows.vue
@@ -0,0 +1,93 @@
+<template>
+  <main id="maincontent">
+    <section id="shows">
+      <h2>
+        <span class="title__left">
+          <img src="../assets/content/stick-left.svg" alt>
+        </span>
+        8 BALL SHOWS
+        <span class="title__right">
+          <img src="../assets/content/stick-right.svg" alt>
+        </span>
+      </h2>
+      <ul class="shows">
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+          <div class="show__info">
+            <span class="show__time">2 pm</span>
+            <h3 class="show__name">MAGIC MARMALADE</h3>
+            <p class="show__day">MONDAYS</p>
+            <p class="show__tags">HIP-HOP - BEATS - JAZZ</p>
+          </div>
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+          <div class="show__info">
+            <span class="show__time">2 pm</span>
+            <h3 class="show__name">MAGIC MARMALADE</h3>
+            <p class="show__day">MONDAYS</p>
+            <p class="show__tags">HIP-HOP - BEATS - JAZZ</p>
+          </div>
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+          <div class="show__info">
+            <span class="show__time">2 pm</span>
+            <h3 class="show__name">MAGIC MARMALADE</h3>
+            <p class="show__day">MONDAYS</p>
+            <p class="show__tags">HIP-HOP - BEATS - JAZZ</p>
+          </div>
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/profile/6/a/5/b/ef32-0a28-481b-928c-2be8447aeb0d"
+            alt="show name"
+          >
+          <div class="show__info">
+            <span class="show__time">2 pm</span>
+            <h3 class="show__name">MAGIC MARMALADE</h3>
+            <p class="show__day">MONDAYS</p>
+            <p class="show__tags">HIP-HOP - BEATS - JAZZ</p>
+          </div>
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+          <div class="show__info">
+            <span class="show__time">2 pm</span>
+            <h3 class="show__name">MAGIC MARMALADE</h3>
+            <p class="show__day">MONDAYS</p>
+            <p class="show__tags">HIP-HOP - BEATS - JAZZ</p>
+          </div>
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/profile/6/a/5/b/ef32-0a28-481b-928c-2be8447aeb0d"
+            alt="show name"
+          >
+          <div class="show__info">
+            <span class="show__time">2 pm</span>
+            <h3 class="show__name">MAGIC MARMALADE</h3>
+            <p class="show__day">MONDAYS</p>
+            <p class="show__tags">HIP-HOP - BEATS - JAZZ</p>
+          </div>
+        </li>
+      </ul>
+      <div class="arrows">
+        <span class="arrow">&#60;</span>
+        <span class="arrow">&#62;</span>
+      </div>
+    </section>
+  </main>
+</template>
