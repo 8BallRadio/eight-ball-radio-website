e6f5635a1d8f1aff2c92051170f13a837f5fbe1b
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-11 05:26:15 -0500

    Create Show.vue and _show.scss files

---
+++ src/views/Show.vue
@@ -0,0 +1,103 @@
+<template>
+  <main id="maincontent">
+    <section id="inside-show">
+      <div class="back">
+        <router-link to="/shows" class="btn back__btn">BACK TO LIST</router-link>
+      </div>
+      <div class="show__image">
+        <img src="images/show.png">
+      </div>
+      <h2>
+        <span class="title__left">
+          <img src="../assets/content/stick-left.svg" alt>
+        </span>
+        SHOWS ARCHIVES
+        <span class="title__right">
+          <img src="../assets/content/stick-right.svg" alt>
+        </span>
+      </h2>
+      <ul class="archive-shows">
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/profile/6/a/5/b/ef32-0a28-481b-928c-2be8447aeb0d"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/profile/6/a/5/b/ef32-0a28-481b-928c-2be8447aeb0d"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/profile/6/a/5/b/ef32-0a28-481b-928c-2be8447aeb0d"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/profile/6/a/5/b/ef32-0a28-481b-928c-2be8447aeb0d"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/profile/6/a/5/b/ef32-0a28-481b-928c-2be8447aeb0d"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+        </li>
+        <li class="show">
+          <img
+            src="https://thumbnailer.mixcloud.com/unsafe/320x320/extaudio/a/6/9/4/13b2-ef57-4fd8-910d-f66253cf7da8"
+            alt="show name"
+          >
+        </li>
+      </ul>
+      <div class="more-archives">
+        <a
+          href="https://www.mixcloud.com/8ballradio/"
+          class="btn more__btn"
+          target="_blank"
+          rel="noopener"
+        >MORE ARCHIVES</a>
+      </div>
+    </section>
+  </main>
+</template>
