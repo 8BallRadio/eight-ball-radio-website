0cf5c2a6f607866f61e2c218b42c9584eeff335f
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-12 09:53:30 -0500

    Initial Contact.vue and contact.scss

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,41 @@
 <template>
-    <div class="contact">
-        Hello
-    </div>
+  <main id="maincontent">
+    <section id="contact">
+      <h2>
+        <span class="title__left">
+          <img src="../assets/content/wave-gray-left.svg" alt>
+        </span>
+        CONTACT
+        <span class="title__right">
+          <img src="../assets/content/wave-gray-right.svg" alt>
+        </span>
+      </h2>
+      <form name="contact" method="POST" data-netlify="true" class="contact-form">
+        <p>
+          <label class="sr-only" for="name">Your Name:</label>
+          <input type="text" name="name" id="name" placeholder="YOUR NAME">
+        </p>
+        <p>
+          <label class="sr-only" for="email">Your Email:</label>
+          <input type="email" name="email" id="email" placeholder="YOUR EMAIL">
+        </p>
+        <p>
+          <label class="sr-only" for="subject">Subject:</label>
+          <input type="text" name="subject" id="subject" placeholder="SUBJECT">
+        </p>
+        <p>If you have question about our site, shows inquiries or have found a bug on our website, please contact us with your details. We will review your message as soon as we can.</p>
+        <p>
+          <label class="sr-only" for="message">Message:</label>
+          <textarea name="message" id="message" rows="12"></textarea>
+        </p>
+        <button type="submit" class="contact__btn">SEND</button>
+      </form>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
-  name: "Contact",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
-  }
+  name: "Contact"
 };
 </script>
