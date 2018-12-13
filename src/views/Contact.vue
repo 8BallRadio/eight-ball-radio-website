9ccae3fdb1a2dd56cf6eb6c1f46ae02250e2b8e0
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-13 08:44:55 -0500

    Add classes and scroll top in the  mounted hook

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -11,22 +11,28 @@
         </span>
       </h2>
       <form name="contact" method="POST" data-netlify="true" class="contact-form">
-        <p>
+        <p class="contact__name">
           <label class="sr-only" for="name">Your Name:</label>
           <input type="text" name="name" id="name" placeholder="YOUR NAME">
         </p>
-        <p>
+        <p class="contact__email">
           <label class="sr-only" for="email">Your Email:</label>
           <input type="email" name="email" id="email" placeholder="YOUR EMAIL">
         </p>
-        <p>
+        <p class="contact__subject">
           <label class="sr-only" for="subject">Subject:</label>
           <input type="text" name="subject" id="subject" placeholder="SUBJECT">
         </p>
         <p>If you have question about our site, shows inquiries or have found a bug on our website, please contact us with your details. We will review your message as soon as we can.</p>
-        <p>
+        <p class="contact__message">
           <label class="sr-only" for="message">Message:</label>
-          <textarea name="message" id="message" rows="12"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            rows="12"
+            maxlength="500"
+            placeholder="TYPE YOUR MESSAGE. PLEASE LIMIT  TO 500 CHARACTERS OR LESS"
+          ></textarea>
         </p>
         <button type="submit" class="contact__btn">SEND</button>
       </form>
@@ -36,6 +42,9 @@
 
 <script>
 export default {
-  name: "Contact"
+  name: "Contact",
+  mounted() {
+    window.scroll(0, 0);
+  }
 };
 </script>
