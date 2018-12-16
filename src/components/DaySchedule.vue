90b5df478d3da4f44c5cfac27dfc1cc706b75963
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-16 15:46:15 -0500

    Initial DaySchedule.vue

---
+++ src/components/DaySchedule.vue
@@ -0,0 +1,33 @@
+<template>
+  <tbody>
+    <tr v-for="(show,index) in shows" :key="index">
+      <td>{{show.starts | showTime}}-{{show.ends | showTime}}</td>
+      <td colspan="3">Show #{{index + 1}} {{day}} - {{show.name}}</td>
+      <td colspan="3">Talk, Indie, Psychdelic</td>
+    </tr>
+  </tbody>
+</template>
+<script>
+export default {
+  props: {
+    shows: {
+      type: Array,
+      required: true
+    },
+    day: {
+      //temporal var. Remove it when we get the real data
+      type: String
+    }
+  },
+  filters: {
+    showTime: function(value) {
+      if (!value) return "";
+      return value
+        .split(" ")[1]
+        .split(":")
+        .slice(0, 2)
+        .join(":");
+    }
+  }
+};
+</script>
