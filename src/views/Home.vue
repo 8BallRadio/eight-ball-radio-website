6ecba11457ee7532ce72327e7e6ab65fe456de9c
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-12-20 20:16:38 +0200

    Add LatestShows component and axios for the latest shows

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,23 +30,48 @@
           <img src="../assets/content/stick-right.svg" alt>
         </span>
       </h2>
-      <ul class="latest-shows">
-        <li class="show">show button to mixcloud player</li>
-        <li class="show">show button to mixcloud player</li>
-        <li class="show">show button to mixcloud player</li>
-        <li class="show">show button to mixcloud player</li>
-        <li class="show">show button to mixcloud player</li>
-        <li class="show">show button to mixcloud player</li>
-      </ul>
+      <div
+        v-if="errored"
+        class="error__msg"
+      >We're sorry, we're not able to retrieve this information at the moment, please try back later</div>
+      <div class="latest-shows__container" v-else>
+        <div v-if="loading">Loading...</div>
+        <latest-shows :shows="shows" v-else></latest-shows>
+      </div>
     </section>
   </main>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import LatestShows from "@/components/LatestShows.vue";
 
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    "latest-shows": LatestShows
+  },
+  data() {
+    return {
+      shows: null,
+      loading: true,
+      errored: false
+    };
+  },
+  mounted() {
+    window.scroll(0, 0);
+    axios
+      .get("https://api.mixcloud.com/8ballradio/cloudcasts/?limit=6")
+      .then(response => this.getShows(response.data))
+      .catch(error => {
+        console.log(error), (this.errored = true);
+      })
+      .finally(() => (this.loading = false));
+  },
+  methods: {
+    getShows(res) {
+      this.shows = res.data;
+    }
+  }
 };
 </script>
