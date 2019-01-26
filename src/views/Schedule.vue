94cdf34e18cc1926d2c9c0738e7f651cd31ee45c
julisbf <bfjuliana@gmail.com>
2019-01-25 22:43:20 -0500

    Update url api

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -65,7 +65,7 @@ export default {
   mounted() {
     window.scroll(0, 0);
     axios
-      .get("//sourcefabric.airtime.pro/api/week-info")
+      .get("//eightball.airtime.pro/api/week-info")
       .then(response => this.getSchedule(response.data))
       .catch(error => {
         console.log(error), (this.errored = true);
