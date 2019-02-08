71182e0698f110a3de6ee50ea63b6ca0b896ec72
julisbf <bfjuliana@gmail.com>
2019-02-07 23:19:23 -0500

    Create SlideShow component

---
+++ src/components/SlideShow.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="slideshow">
+    <li v-for="(collection, key) in collections" :key="key">
+      {{collection.url}}
+      <img :alt="collection.alt" :src="collection.url" class="slideshow__image">
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    collections: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
