13f9ef18684c1c380139ca8f22763c711c959801
Austin <autbrown@ucsc.edu>
2019-04-04 20:06:48 -0400

    Add PreloadWebpackPlugin to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const PreloadWebpackPlugin = require("@vue/preload-webpack-plugin")
 
 module.exports = {
   pluginOptions: {
@@ -6,5 +7,13 @@ module.exports = {
       preProcessor: "scss",
       patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new PreloadWebpackPlugin({
+        rel: "preload",
+        include: "all"
+      })
+    ]
   }
 };
