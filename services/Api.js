bdfcebe77353253ea9f05004cd4eaf2e8876333d
evansavage <esav678@gmail.com>
2025-10-10 14:02:53 -0400

    Add API endpoint consumption from env variables

--- services/Api.js
+++ services/Api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
-export default() => {
+export default () => {
   return axios.create({
-    baseURL: 'https://eightballradio-backend.herokuapp.com/'
+    baseURL: process.env.VUE_APP_API_URL || "http://127.0.0.1:8081",
   });
-}
+};
